d33f94a48f423850cd3e1fe1a1212ed815f0fc5a
Kris465 <nataza7394@gmail.com>
2025-10-01 11:41:01 +0300

    соломахин

---
+++ block1/task2/task2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+int main() {
+
+    std::cout << std::fixed << std::setprecision(1);
+
+    std::cout << "Число e: " << M_E << std::endl;
+
+    return 0;
+}
---
+++ block1/task3/task3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+   
+    int number;
+    
+    std::cout << "Введите число: ";
+    
+    std::cin >> number;
+    
+    std::cout << "Вы ввели число " << number << std::endl;
+    
+    return 0;
+}
