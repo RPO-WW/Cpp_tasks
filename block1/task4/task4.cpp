06960a7db526fb22b787038e16087e1be5968ab4
Kris465 <nataza7394@gmail.com>
2025-10-01 11:57:56 +0300

    Манираки

---
+++ block1/task4/task4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    double number;
+    std::cout << "Введите число: ";
+    std::cin >> number;
+    std::cout << "Число: " << number;
+    std::cout << " Вот какое число вы ввели)" << std::endl;
+    return 0;
+}
