5d9ea748b2366a397a713826e06e298034d14465
Kris465 <nataza7394@gmail.com>
2025-10-01 13:35:01 +0300

    Манираки

---
+++ block1/task11/task11.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+main() {
+    int number0;
+    std::cout << "Введите нулевое число: ";
+    std::cin >> number0;
+    
+    int number1;
+    std::cout << "Ввдеите первое число: ";
+    std::cin >> number1;
+
+    int number2;
+    std::cout << "Введите второк число: ";
+    std::cin >> number2;
+    
+    int number3;
+    std::cout << "Ввдеите третье число: ";
+    std::cin >> number3;
+
+    std::cout << number0 << std::endl;
+    std::cout << number1 << std::endl;
+    std::cout << number2 << std::endl;
+    std::cout << number3 << std::endl;
+}
