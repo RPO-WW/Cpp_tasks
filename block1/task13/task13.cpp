26a59a73d16e1440a7e109a51195b297537e72db
Kris465 <nataza7394@gmail.com>
2025-10-01 13:40:03 +0300

    соломахин

---
+++ block1/task13/task13.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    int a, b, x, y;
+    
+
+    std::cout << "Введите значения a, b, x, y через пробел: ";
+    std::cin >> a >> b >> x >> y;
+    
+
+    std::cout << "\nа)" << std::endl;
+    std::cout << "2 кг" << std::endl;
+    std::cout << "13 17" << std::endl;
+    
+    std::cout << "\nб)" << std::endl;
+    std::cout << a << " 1" << std::endl;
+    std::cout << "19 " << b << std::endl;
+    
+    std::cout << "\nв)" << std::endl;
+    std::cout << x << " " << y << std::endl;
+    std::cout << "5 " << y << std::endl;
+    
+    return 0;
+}
