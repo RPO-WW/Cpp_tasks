b71e08e0500e1770439ce6a77cf1315b6a048ade
Durak2000 <fetisovtimur0@gmail.com>
2025-10-02 13:35:40 +0300

    solomakhin

---
+++ block1/task18/task18.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    double s;
+    double k;
+
+
+    s = 5;
+    s = 57;
+    cout << "a) s = " << s << std::endl;
+
+
+    double s = 6;
+    s = -5.2 * s;
+    s = 0;
+    std::cout << "б) s = " << s << std::endl;
+
+
+    s = -7.5;
+    s = 2 * s;
+    cout << "в) s = " << s << std::endl;
+
+
+    s = 45;
+    s = -25;
+
+
+    k = 10;
+    s = s + k;
+     cout << "в) s = " << s << "при k = " << k << ")" << std:: endl;
+
+     return 0;
+    }
