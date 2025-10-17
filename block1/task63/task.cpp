7b24109219093135e9a6d44686944c112dec985d
Durak2000 <fetisovtimur0@gmail.com>
2025-10-17 10:21:47 +0300

    kirill

---
+++ block1/task63/task.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    int n;
+    std::cout << "Введите натуральное число n (n < 9999): ";
+    std::cin >> n;
+
+    int d1 = n / 1000;
+    int d2 = (n / 100) % 10;
+    int d3 = (n / 10) % 10;
+    int d4 = n % 10;
+
+    if (d1 == d2 || d1 == d3 || d1 == d4 || d2 == d3 || d2 == d4 || d3 == d4) {
+        std::cout << "Цифры числа не различны." << std::endl;
+    } else {
+        std::cout << "Все цифры числа различны." << std::endl;
+    }
+
+    return 0;
+}
