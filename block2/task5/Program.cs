b31d0cbf6663c2f3c5a51fc40afc20f3d701d7bc
Kris465 <nataza7394@gmail.com>
2025-10-21 11:45:16 +0300

    popov

---
+++ block2/task5/Program.cs
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+
+int main() {
+    int days = 234;
+    int fullWeeks = days / 7;
+    std::cout << "Прошло " << fullWeeks << " полных недель." << std::endl;
+    return 0;
+}
