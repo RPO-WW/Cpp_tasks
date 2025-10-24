8daa9a6113b7f5f8af4faa15d5c28806055294a5
Kris465 <nataza7394@gmail.com>
2025-10-24 13:47:06 +0300

    Манираки

--- block2/task14/task14.cpp
+++ block2/task14/task14.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <stdexcept>
 using namespace std;
 
 int main() {
@@ -8,11 +7,15 @@ int main() {
         cout << "Ввдеите трёхзначное число" << endl;
         cin >> tri;
 
-        int tri_0 = tri / 100;
-        int tri_1_2 = tri % 100;
-        int itog = (tri_1_2 * 10) + tri_0;
-
-        cout << itog << endl;
+        if(tri < 1000 && tri > 99) {
+            int tri_0 = tri / 100;
+            int tri_1_2 = tri % 100;
+            int itog = (tri_1_2 * 10) + tri_0;
+            cout << itog << endl;
+        }
+        else {
+            cerr << "Должно быть трёхзначное число" << endl;
+        }
     }
     catch(exception e) {
         cerr << "Ошибка" << endl;
