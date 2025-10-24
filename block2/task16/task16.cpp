57b7afca52afdb34b333238b0eea29d4ab17a085
Kris465 <nataza7394@gmail.com>
2025-10-24 14:08:30 +0300

    linda

--- block2/task16/task16.cpp
+++ block2/task16/task16.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <iomanip>
-#include <cmath>
+#include <cmath>  // Добавляем для fmod
 using namespace std;
 
 int main() {
     int h, m, s;
     
-  
     cout << "Введите часы (0 < h ≤ 23): ";
     cin >> h;
     
@@ -16,18 +15,16 @@ int main() {
     cout << "Введите секунды (0 ≤ s ≤ 59): ";
     cin >> s;
     
-    
     if (h <= 0 || h > 23 || m < 0 || m > 59 || s < 0 || s > 59) {
         cout << "Ошибка: некорректный ввод времени!" << endl;
         return 1;
     }
     
+    int hours12 = h % 12;
+    double angle = hours12 * 30 + m * 0.5 + s * (0.5 / 60);
     
-    double totalSeconds = h * 3600 + m * 60 + s;
-    
-   
-    double angle = fmod(totalSeconds, 43200.0) * 360.0 / 43200.0;
-    
+    // Используем fmod с <cmath>
+    angle = fmod(angle, 360.0);
     
     cout << "Время: " 
          << setw(2) << setfill('0') << h << ":" 
@@ -35,11 +32,6 @@ int main() {
          << setw(2) << setfill('0') << s << endl;
     cout << fixed << setprecision(2);
     cout << "Угол часовой стрелки: " << angle << "°" << endl;
-    
-  
-    cout << "\nДетали расчета:" << endl;
-    cout << "Всего секунд: " << totalSeconds << endl;
-    cout << "Секунд в 12-часовом цикле: " << fmod(totalSeconds, 43200.0) << endl;
     
     return 0;
 }
